cc492b842edb9eb14da87203d140661af66ccbbb
psdaniel <daniel.n10@hotmail.com>
2021-07-09 17:37:23 -0300

    Remove get handler

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,6 @@ import { postsRoutes } from './routes/posts';
 const app = express();
 app.use(express.json())
 
-app.get('/', async (req, res) => {
-  return res.json({ message: 'Hello world' });
-});
-
 app.use(postsRoutes);
 
 app.listen(3333);
