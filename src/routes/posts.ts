3217a76fbb523606fde4ada0630d5fdcc5650b3d
psdaniel <daniel.n10@hotmail.com>
2021-07-11 16:15:40 -0300

    Adjust error message

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -3,12 +3,18 @@ import { getUsers, filterUserByCompany, getPosts, filterData } from '../utils';
 
 const router = Router();
 
-router.post('/api/posts', async (req, res) => {
+router.post('/api/posts-company', async (req, res) => {
     const users = await getUsers();
 
     const filteredUser = filterUserByCompany(users, req.body);
 
     const posts = await getPosts();
+
+    if (filteredUser.length === 0) {
+        return res.status(400).send({
+            message: 'Não foi encontrado nenhum usuário com o nome dessa empresa!'
+        });
+    }
 
     const filteredData = filterData(filteredUser, posts);
 
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { postsRoutes } from './routes/posts';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json())
+app.use(cors())
 
 app.use(postsRoutes);
 
